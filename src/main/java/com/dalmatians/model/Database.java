d2f75587ea340dd1a95d94a8f63b9283950c65ca
Sebastian García Acosta <segaracos@outlook.com>
2020-11-01 13:40:13 -0500

    DAtabase skeleton done

--- src/main/java/com/dalmatians/model/Database.java
+++ src/main/java/com/dalmatians/model/Database.java
@@ -1,5 +1,52 @@
 package com.dalmatians.model;
 
 public class Database {
-
+	
+	public enum CRITERION {
+		NAME,
+		FULLNAME,
+		ID,
+		SURNAME
+	}
+	
+	enum EDITABLE_ATTRIBUTE {
+		NAME,
+		FULLNAME,
+		SURNAME,
+		SEX,
+		BIRTHDATE,
+		HEIGHT
+	}
+	
+	public void generate(int n) {
+	}
+	
+	public static void load(String path) {
+	
+	}
+	
+	public void save(String path) {
+		
+	}
+	
+	public Person createRandomPerson() {
+		return null;
+	}
+	
+	public Person search(CRITERION criteria, String value) {
+		return null;
+	}
+	
+	public void delete(Person p) {
+		
+	}
+	
+	public <T> void update(EDITABLE_ATTRIBUTE attr, T newValue) {
+		
+	}
+	
+	public double getRandomHeight(double mean, double stdv) {
+		return 0.0;
+	}
+	
 }
